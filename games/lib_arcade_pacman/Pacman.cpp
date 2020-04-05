40ef917a4905da42c0c5f4318d69b6579108f110
Lal <lal.joncoux-aydin@epitech.eu>
2020-04-05 23:04:41 +0200

    [ADD] Bonus

--- games/lib_arcade_pacman/Pacman.cpp
+++ games/lib_arcade_pacman/Pacman.cpp
@@ -139,15 +139,17 @@ int Pacman::move_player(int x, int y)
         _player.pickObject();
     }
 
-    // collision with big objects for 10 sec
-    // if (config_file[new_player_x][new_player_y] == '%') {
-    //     _player.setInterract(true);
-    // }
-    // if (_player._interract == true)
-    //     _bonus += 1;
-    // if (_bonus == "1000")
-    //     _player.setInterract(false);
-
+    if (config_file[new_player_x][new_player_y] == '_') {
+        _player.setInterract(true);
+        _player.pickObject();
+    }
+    if (_player._interract == true) {
+        _bonus += 1;
+        if (_bonus == 150) {
+            _player.setInterract(false);
+            _bonus = 0;
+        }
+    }
     // collision with ennemies
     if (config_file[new_player_x][new_player_y] == 'E') {
         if (_player._interract == false) {
@@ -155,8 +157,21 @@ int Pacman::move_player(int x, int y)
             config_file.clear();
             config_file = readSceneFile();
             return 0;
+        } else if (_player._interract == true) {
+            // ENNEMIES GET DAMAGE
+            // loop in ennemies array
+            // for (size_t i = 0; i < ennemies_list.size(); i++) {
+            //     if (ennemies_list[i].pos_x == new_player_x && ennemies_list[i].pos_y == new_player_y) {
+            //         std::cout << new_player_x << std::endl;
+            //         std::cout << new_player_y << std::endl;
+            //         // ennemies.getDammage();
+            //         // ennemies.setPosX(ennemie_out_x);
+            //         // ennemies.setPosY(ennemie_out_t);
+            //     }
+            // }
         }
     }
+
 
     // Teleport to the other side of the map
     if (config_file[new_player_x][new_player_y] == '!') {
@@ -172,7 +187,7 @@ int Pacman::move_player(int x, int y)
     // collision with walls
     if ((config_file[new_player_x][new_player_y] == 'E' && _player._interract == true) ||
     config_file[new_player_x][new_player_y] == '0' ||
-    config_file[new_player_x][new_player_y] == ' ') {
+    config_file[new_player_x][new_player_y] == ' ' || config_file[new_player_x][new_player_y] == '_') {
             config_file[_player.pos_x][_player.pos_y] = ' ';
             _player.pos_x = new_player_x;
             _player.pos_y = new_player_y;
@@ -189,6 +204,8 @@ void Pacman::ia_ennemy()
     for (size_t i = 0; i < ennemies_list.size(); i++) {
         if (ennemies_list[i].e_time == out_time[i]) {
             config_file[ennemies_list[i].home_x][ennemies_list[i].home_y] = ' ';
+            ennemies_list[i].setPosX(14);
+            ennemies_list[i].setPosY(52);
             config_file[ennemies_list[i].pos_x][ennemies_list[i].pos_y] = 'E';
             ennemies_list[i].e_time = ennemies_list[i].e_time + 1;
         } else if (ennemies_list[i].e_time > out_time[i]) {
